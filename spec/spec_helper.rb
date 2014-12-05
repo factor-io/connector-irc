6eae5b675912569514b34c206764d31391ca0428
Maciej Skierkowski <maciej@skierkowski.com>
2014-12-05 09:57:15 -0800

    Adding code climate reporting support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
+require "codeclimate-test-reporter"
 require 'rspec'
 require 'factor-connector-api/test'
+
+CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
 
 Dir.glob('./lib/factor/connector/*.rb').each { |f| require f }
 
