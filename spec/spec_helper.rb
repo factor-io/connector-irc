a094b7b45c542cb963501f8fc29a941cd1dfa5a0
Maciej Skierkowski <maciej@skierkowski.com>
2014-12-05 09:50:16 -0800

    fix reference

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'factor-connector-api/test'
 
-Dir.glob('../lib/factor/connector/*.rb').each { |f| require f }
+Dir.glob('./lib/factor/connector/*.rb').each { |f| require f }
 
 RSpec.configure do |c|
   c.include Factor::Connector::Test
