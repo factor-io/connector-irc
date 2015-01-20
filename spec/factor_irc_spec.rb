d5a9a291079aca1d2392ce429cd3b56fcb70e81a
Maciej Skierkowski <maciej@skierkowski.com>
2015-01-19 16:55:18 -0800

    fix test

--- spec/factor_irc_spec.rb
+++ spec/factor_irc_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
-describe 'IRC' do
-  describe 'Messages' do
-    it 'can send a message' do
-      test_instance = service_instance('irc')
+describe 'irc' do
+  describe ':: messages' do
+    before do
+      @scope = service_instance('irc_messages')
+    end
+
+    it ':: send' do
 
       params = {
         'server'  => 'irc.freenode.net',
@@ -12,18 +15,8 @@ describe 'IRC' do
         'user'    => 'fiobot'
       }
 
-      test_instance.test_action('send',params) do
-        expect_info message: "IRC: Connecting to #{params['server']}..."
-        expect_info message: "IRC: Logging in as #{params['user']}"
-        expect_info message: "IRC: Joining channel #{params['channel']}"
-        expect_info message: "IRC: Successfully joined #{params['channel']}"
-        expect_info message: "IRC: Sending message: #{params['message']}"
-        expect_info message: 'IRC: Message sent, exiting'
-
-        response = expect_return
-        expect(response[:payload]).to be_a(Hash)
-        expect(response[:payload]['message']).to be_a(String)
-        expect(response[:payload]['message']).to eq params['message']
+      @scope.test_action('send',params) do
+        expect_return
       end
     end
   end
